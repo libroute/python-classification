20e1a4fed3d981981171fa0834037b24b1d5c9c8
David Lisk <david.lisk@xsresearch.co.uk>
2017-12-06 00:57:21 +0000

    Added classifier based on scikit-neuralnetwork
    Signed-off-by: David Lisk <david.lisk@xsresearch.co.uk>

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.6
+
+RUN useradd -m user
+
+WORKDIR /home/user
+USER user
+
+RUN pip install scikit-neuralnetwork --user
+RUN pip uninstall --yes Theano
+RUN pip install Theano==0.7 --user
+RUN pip install numpy --user
+
+RUN mkdir /home/user/data
+WORKDIR /home/user/data
+CMD ["python","classify.py"]
---
+++ classify.py
@@ -0,0 +1,26 @@
+# A neural network classifier in python using scikit-neuralnetwork
+# Assumes two input variables and three output classes
+
+params = {}
+with open("params.txt") as f:
+    for line in f:
+        name, var = line.partition("=")[::2]
+        params[name.strip()] = int(var)
+
+from numpy import genfromtxt
+data = genfromtxt('train.csv', delimiter=',')
+
+from sknn.mlp import Classifier, Layer
+nn = Classifier(layers=[Layer("Tanh",units=5),Layer("Sigmoid",units=5),Layer("Softmax")],learning_rate = 0.01, n_iter=500)
+nn.fit(data[0::10,params['n_truth']:], data[0::10,0:params['n_truth']])
+
+data_test = genfromtxt('test.csv', delimiter=',')
+outp=nn.predict(data_test)
+print(data_test.shape)
+print(outp.shape)
+from numpy import concatenate
+
+data_out = concatenate((outp,data_test), axis=1)
+
+from numpy import savetxt
+savetxt("test_output.csv", data_out, delimiter=",")

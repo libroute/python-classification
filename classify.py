f9312febd29689063c46bc718e13f5086ff9ea5e
David Lisk <david.lisk@xsresearch.co.uk>
2017-12-17 23:32:48 +0000

    Train on all data
    Signed-off-by: David Lisk <david.lisk@xsresearch.co.uk>

--- classify.py
+++ classify.py
@@ -12,7 +12,7 @@ data = genfromtxt('train.csv', delimiter=',')
 
 from sknn.mlp import Classifier, Layer
 nn = Classifier(layers=[Layer("Tanh",units=5),Layer("Sigmoid",units=5),Layer("Softmax")],learning_rate = 0.01, n_iter=500)
-nn.fit(data[0::10,params['n_truth']:], data[0::10,0:params['n_truth']])
+nn.fit(data[:,params['n_truth']:], data[:,0:params['n_truth']])
 
 data_test = genfromtxt('test.csv', delimiter=',')
 outp=nn.predict(data_test)
